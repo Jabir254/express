08517de865ce481ebf76b271a96dea1d052d68fc
jabir254 <jabirkullow@gmail.com>
2024-01-18 20:02:58 +0300

    app get

---
+++ src/index.mjs
@@ -0,0 +1,38 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+const mockUsers = [
+  {id: 1, username:"jabir", displayName: "Jabir"},
+  {id:2, username:"Kullow", displayName:"Kullow"},
+  {id:3, username:"fade", displayName:"Fathe"}];
+
+app.get('/', (req, res) => {
+  res.send('<h3>Welcome home</h3>')
+});
+app.get('/api/users', (req, res) => {
+  res.send(mockUsers)
+});
+
+app.get('/api/users/:id', (req, res) => {
+  
+  const parsedId = parseInt(req.params.id);
+  
+  if (isNaN(parsedId)) 
+    return res.status(400).send({msg: "bad request"});
+
+  const findUser = mockUsers.find((user) => user.id === parsedId);
+  if (!findUser) return res.sendStatus(404);
+  return res.send(findUser);
+});
+
+app.get('/api/products', (req,res) => {
+  res.send([{id:123, name:"Tomato", price:345},])
+});
+
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}`)
+});
+
