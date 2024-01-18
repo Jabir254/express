bfbfd9ecc928911b54f66bc934d1767d7086c41f
jabir254 <jabirkullow@gmail.com>
2024-01-18 20:17:52 +0300

    updated

--- src/index.mjs
+++ src/index.mjs
@@ -7,12 +7,17 @@ const PORT = process.env.PORT || 3000;
 const mockUsers = [
   {id: 1, username:"jabir", displayName: "Jabir"},
   {id:2, username:"Kullow", displayName:"Kullow"},
-  {id:3, username:"fade", displayName:"Fathe"}];
+  {id:3, username:"fade", displayName:"Fathe"},
+  {id:4, username:"jason", displayName:"Jason"},
+  {id:5, username:"henry", displayName:"Henry"},
+  {id:6, username:"tina", displayName:"Tina"},
+  {id:7, username:"marilyn", displayName:"Marilyn"}];
 
 app.get('/', (req, res) => {
   res.send('<h3>Welcome home</h3>')
 });
 app.get('/api/users', (req, res) => {
+  console.log(req.query);
   res.send(mockUsers)
 });
 
