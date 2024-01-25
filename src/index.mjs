2f8ed1ead59844d8f98740b64a1c40be2810c1e0
jabir254 <jabirkullow@gmail.com>
2024-01-25 11:54:40 +0300

    added cookie parser and sessions

--- src/index.mjs
+++ src/index.mjs
@@ -1,18 +1,31 @@
 import express from "express";
 import routers from "./routes/index.mjs";
 import cookieParser from "cookie-parser";
+import session from "express-session";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  session({
+    secret: "jabirtag",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 60000 * 60,
+    },
+  }),
+);
 
 app.use(routers);
 
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.cookie("hello", "world", { maxAge: 60000 * 60 * 2 });
+  console.log(req.session);
+  console.log(req.session.id);
+  res.cookie("hello", "world");
   res.status(201).send({ msg: "Hello world" });
 });
 
--- src/routes/products.mjs
+++ src/routes/products.mjs
@@ -3,8 +3,8 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/api/products", (req, res) => {
-  console.log(req.headers.cookie);
-  console.log(req.cookies);
+  // console.log(req.headers.cookie);
+  // console.log(req.cookies);
   if (req.cookies.hello && req.cookies.hello === "world")
     return res.send([{ id: 123, name: "Tomato", price: 345 }]);
   return res.send({ msg: "sorry need the correct cookies" });
